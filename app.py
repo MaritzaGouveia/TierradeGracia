51bec981253e688f9b09b2e1c7bfce62a7638646
Maritza De jesus <maritzadejesus@Maritzas-MacBook-Air.local>
2026-08-11 20:17:46 +0100

    correcciones para deploy

--- app.py
+++ app.py
@@ -216,13 +216,10 @@ from supabase import create_client
 from dotenv import load_dotenv
 
 load_dotenv()
+SUPABASE_URL = st.secrets.get("SUPABASE_URL")
+SUPABASE_KEY = st.secrets.get("SUPABASE_KEY")
 
 
-
-SUPABASE_URL = st.secrets.get("SUPABASE_URL") or os.getenv("SUPABASE_URL")
-SUPABASE_KEY = st.secrets.get("SUPABASE_KEY") or os.getenv("SUPABASE_KEY")
-
-@st.cache_resource
 def get_supabase_client():
     return create_client(SUPABASE_URL, SUPABASE_KEY)
 
